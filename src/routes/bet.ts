09141f909591cb9dd0a7318ea68db03a1209e436
nicholaschun <nicholaschunrayne@gmail.com>
2024-12-19 01:46:30 +0000

    add jsdoc

--- src/routes/bet.ts
+++ src/routes/bet.ts
@@ -2,8 +2,15 @@ import express, { type Router, type Response, type Request } from "express";
 
 const routes: Router = express.Router();
 
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Checks health of api
+ *     description: Check to see if api is up and running
+ */
 routes.get("/health", async (req: Request, res: Response) => {
-	res.end("Api is live");
+	res.end("Api is up and running");
 });
 
 export default routes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import express, { type Router } from "express";
+import swaggerUI from "swagger-ui-express";
 import config from "../config";
+import swaggerSpec from "../utils/js-doc";
 import bet from "./bet";
 
 const { baseUrl } = config;
 const router: Router = express.Router();
 
 router.use(`${baseUrl}/bet`, bet);
+router.use(`${baseUrl}/docs`, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 export default router;
